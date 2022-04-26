b5032128319339dd710d4fa8e418dc794c6eebea
IgnacioMGarrido <nama1510@gmail.com>
2022-04-26 02:33:38 +0200

    Created Application

---
+++ src/Application.cpp
@@ -0,0 +1,21 @@
+#include <Legio/application/EntryPoint.h>
+#include <Legio/platform/Log.h>
+#include <string>
+
+class Application : public LG::Application
+{
+public:
+    Application(std::string appName = "Main App") : LG::Application(appName)
+    {
+    }
+
+    virtual ~Application()
+    {
+        LG_APP_INFO("Application Deleted!");        
+    }
+};
+
+LG::Application* CreateApplication()
+{
+    return new Application("Legio Test!");
+}
